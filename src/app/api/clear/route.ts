6a937d4fe8eef4b49bf7d2f7e00abecc1925e57c
Orlox <the1orlox@gmail.com>
2023-08-03 09:49:21 -0400

    clean queues

---
+++ src/app/api/clear/route.ts
@@ -0,0 +1,22 @@
+import { NextRequest } from "next/server";
+import { BuyQueue, REDIS_URL } from "../../../../utilities/constants";
+import Queue from 'bull';
+
+export async function DELETE(request: NextRequest) {
+    await BuyQueue.pause();
+    await BuyQueue.empty();
+    await BuyQueue.resume();
+
+    const workQueue = new Queue('dca', REDIS_URL, {
+        redis: {
+            tls: {
+                rejectUnauthorized: false,
+                requestCert: true,
+            },
+        },
+    });
+
+    await workQueue.pause();
+    await workQueue.empty();
+    await workQueue.resume();
+} 
